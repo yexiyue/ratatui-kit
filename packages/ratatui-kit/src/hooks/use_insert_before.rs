c813fe8953f38d70baf1a575be994f746ddb5862
yexiyue <yexiyue666@qq.com>
2025-07-05 12:31:14 +0800

    feat: insert_before添加render_before方法

--- packages/ratatui-kit/src/hooks/use_insert_before.rs
+++ packages/ratatui-kit/src/hooks/use_insert_before.rs
@@ -1,9 +1,10 @@
 use std::{
     collections::VecDeque,
     sync::{Arc, Mutex},
+    task::{Poll, Waker},
 };
 
-use ratatui::buffer::Buffer;
+use ratatui::{buffer::Buffer, widgets::Widget};
 
 use crate::{Hook, Hooks, Terminal};
 
@@ -17,9 +18,24 @@ type FnBox = Box<dyn FnOnce(&mut Buffer) + Send>;
 #[derive(Clone, Default)]
 pub struct InsertBeforeHandler {
     queue: Arc<Mutex<VecDeque<(u16, FnBox)>>>,
+    waker: Arc<Mutex<Option<Waker>>>,
 }
 
 impl Hook for InsertBeforeHandler {
+    fn poll_change(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context,
+    ) -> std::task::Poll<()> {
+        let mut waker = self.waker.lock().unwrap();
+        let mut queue = self.queue.lock().unwrap();
+        if queue.is_empty() {
+            waker.replace(cx.waker().clone());
+            Poll::Pending
+        } else {
+            Poll::Ready(())
+        }
+    }
+
     fn post_component_update(&mut self, updater: &mut crate::ComponentUpdater) {
         let mut queue = self.queue.lock().unwrap();
         for (height, callback) in queue.drain(..) {
@@ -29,12 +45,26 @@ impl Hook for InsertBeforeHandler {
 }
 
 impl InsertBeforeHandler {
-    pub fn insert_before<F>(&self, height: u16, callback: F)
+    pub fn insert_before<F>(&self, height: u16, callback: F) -> &Self
     where
         F: FnOnce(&mut Buffer) + Send + 'static,
     {
         let mut queue = self.queue.lock().unwrap();
         queue.push_back((height, Box::new(callback)));
+        self
+    }
+
+    pub fn render_before<T: Widget + Send + 'static>(&self, widget: T, height: u16) -> &Self {
+        self.insert_before(height, move |buf| {
+            widget.render(buf.area, buf);
+        });
+        self
+    }
+
+    pub fn finish(&self) {
+        if let Some(waker) = self.waker.lock().unwrap().take() {
+            waker.wake();
+        }
     }
 }
 
