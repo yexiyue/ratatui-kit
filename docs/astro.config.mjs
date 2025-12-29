4654e9ea9e441d9109d3afc03a93309934796e44
yexiyue <yexiyue666@qq.com>
2025-12-29 09:07:51 +0800

    docs: migrate docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,50 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeNova from 'starlight-theme-nova';
+
+// https://astro.build/config
+export default defineConfig({
+	base: "/ratatui-kit",
+	integrations: [
+		starlight({
+			routeMiddleware: './src/routeData.ts',
+			locales: {
+				root: {
+					label: '简体中文',
+					lang: 'zh-CN'
+				},
+				// en: {
+				//     label:'English',
+				//     lang:'en'
+				// }
+			},
+			plugins: [starlightThemeNova({
+				nav: [
+					{
+						label: '文档', href: '/ratatui-kit/guides/intro/'
+					},
+					{
+						label: "原理", href: "/ratatui-kit/principle/01-前言"
+					}
+				]
+			})],
+			title: 'Ratatui Kit',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/yexiyue/ratatui-kit' }],
+			sidebar: [
+				{
+					label: '指南',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: '示例',
+					autogenerate: { directory: 'example' },
+				},
+				{
+					label: '原理',
+					autogenerate: { directory: 'principle' },
+				},
+			],
+		}),
+	],
+});
